148ea686580f26bf4cb7ff7b10f14ea79585d135
372a05 <372a05@users.noreply.github.com>
2015-07-02 12:10:10 -0500

    Update and rename apt.sh to apt.do_update_all.sh

---
+++ ubuntu-config.d/apt.do_update_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# ubuntu-config.d/apt.do_update_all.sh
+# Aptitude Package Manager
+
+id="apt_do_update_all"
+message="Cleanly update repositories, install upgrades, and remove unused packages? [Y/n] "
+choices="y,n"
+default="y"
+
+if[ $input=="y" ]; then
+  echo "Starting apt packages update-all... " 
+  sudo apt-get update -y
+  sudo apt-get dist-upgrade -y
+  sudo apt-get autoremove --purge -y
+  sudo apt-get autoclean -y
+  echo "Finished apt packages update-all. "
+fi
